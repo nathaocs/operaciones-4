b7cbe552dc1b4c13b819f43b31f21c10ee9755ec
JERP000 <jerp000@gmail.com>
2025-04-10 12:58:54 -0500

    Update codigo_3 JERP.py

--- codigo_3/codigo_3.py
+++ codigo_3/codigo_3.py
@@ -1,8 +1,12 @@
 # Estudiante: corrige este código y haz un pull request con la versión corregida.
 def dividir_listas(lista1, lista2):
+    
     resultado = []
     for i in range(len(lista1)):
-        resultado.append(lista1[i] / lista2[i])
+        try:
+            resultado.append(lista1[i] / lista2[i])
+        except ZeroDivisionError:
+            resultado.append(none)
     return resultado
 
 print(dividir_listas([4, 8], [2, 0]))
